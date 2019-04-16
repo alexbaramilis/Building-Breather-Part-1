6c0dcd05df1c3be74120923e885b95b1416e78d8
Alexandros Baramilis <alexbaramilis@hotmail.com>
2019-04-16 17:00:18 +0100

    Readability improvement.

--- Breather Part 1/Supporting Files/Extensions/String+Extension.swift
+++ Breather Part 1/Supporting Files/Extensions/String+Extension.swift
@@ -11,7 +11,8 @@ import Foundation
 extension String {
     /// Returns the indices of all the numbers in the string.
     ///
-    /// If a number is directly preceded by a dot/comma, the index of the dot/comma will also be returned.
+    /// If a number is directly preceded by a dot/comma, the index of the dot/comma
+    /// will also be returned.
     /// This accounts for potential numbers with decimal or thousands separators.
     var indicesOfNumbers: [Int] {
         var indices = [Int]()
@@ -22,11 +23,13 @@ extension String {
             !range.isEmpty
         {
             let index = distance(from: self.startIndex, to: range.lowerBound)
-            if index > 0,
-                let previousCharacter = self[self.index(self.startIndex, offsetBy: index - 1)].unicodeScalars.first,
-                (previousCharacter == "." || previousCharacter == ",")
-            {
-                indices.append(index - 1)
+            if index > 0 {
+                let previousIndex = self.index(self.startIndex, offsetBy: index - 1)
+                if let previousCharacter = self[previousIndex].unicodeScalars.first,
+                    (previousCharacter == "." || previousCharacter == ",")
+                {
+                    indices.append(index - 1)
+                }
             }
             indices.append(index)
             searchStartIndex = range.upperBound
